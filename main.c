7b2984dcf725d47d02aeb1cdc66aace4de4492a5
Alvaro R. S. Dziadzio <dziadzio.alvaro@gmail.com>
2024-12-09 17:14:01 -0300

    Initial Commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <limits.h>
+
+void multi_partition_mpi(long long *input, int n, long long *p, int np, long long *output, int *nO) {
+
+	
+
+}
+
+int main(int argc, char **argv) {
+
+	printf("Hello World\n");
+
+	return 0;
+}
